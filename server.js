7656e1dac3de5ee63c0372652b73997581480297
Matt92Martin <matt92martin@gmail.com>
2021-03-03 21:13:18 -0800

    config changes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const webpack = require('webpack')
 const cookieParser = require('cookie-parser')
 const middleware = require('webpack-dev-middleware')
 const compiler = webpack(require('./webpack.config'))
-const { cookieSecret } = require('./config')
+const { cookieSecret, PORT } = require('./config')
 
 const isDev = process.env.NODE_ENV === 'development'
 
@@ -81,6 +81,6 @@ io.on('connection', (socket) => {
     })
 })
 
-server.listen(3000, () => {
+server.listen(PORT, () => {
     console.log('Listening on http://localhost:3000')
 })
